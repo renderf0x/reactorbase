03cefd804cbb7e22be08e543aa4df10a11852cf3
Derek Barncard <derek@barncard.com>
2014-10-21 15:39:49 -0700

    added db config line

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //basic global config
 
 module.exports = {
-	db_url: 'mongodb://localhost:27017/hackertest',
+	db_url: (process.env.MONGO_URL || 'mongodb://localhost:27017/hackertest'),
 	listen_port: '3000'
 }
