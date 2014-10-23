9252deb7b27ba72b669bc48c01a2faa2d5c2aa16
Derek Barncard <derek@barncard.com>
2014-10-23 11:36:16 -0700

    uncommented data import lines

--- server.js
+++ server.js
@@ -50,7 +50,8 @@ app.use('/', router);
 app.listen(process.env.PORT || 3000);
 
 //uncomment for loading new data from data.js
-
+/*
 var hackerImport = require('./import/hacker_import');
 
 hackerImport.loadHackerArray(hackerImport.data);
+*/
